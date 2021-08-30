b72b173c125a2d791397c03460af3d2d3f323705
shristyguptaa <89759891+shristyguptaa@users.noreply.github.com>
2021-08-30 09:27:09 +0530

    Create print number in reverse order.cpp

---
+++ print number in reverse order.cpp
@@ -0,0 +1,13 @@
+//WAP to print number in reverse order 
+#include<stdio.h>
+int main(){
+	int num = 123573145;
+	int rnum=0,rem;
+	while (num != 0){
+		rem = num%10;
+		rnum = (rnum*10) + rem;
+		num /= 10;
+	}
+	printf("Reversed number is %d",rnum);
+	return 0;
+}
