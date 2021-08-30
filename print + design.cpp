ccc6091c9db0095488fe6ec5d7e7d1af4cd40ed3
shristyguptaa <89759891+shristyguptaa@users.noreply.github.com>
2021-08-30 16:07:05 +0530

    Create print + design.cpp

---
+++ print + design.cpp
@@ -0,0 +1,16 @@
+//write a program for + design
+
+#include<stdio.h>
+int main(){
+	for(int i = 1; i <= 5; i++){
+		for(int j = 1; j <= 5;j++){
+			if (i == 3||j == 3){
+				printf("*");
+			}
+			else 
+			printf(" ");
+		}
+		printf("\n");
+	}
+	return 0;
+}
