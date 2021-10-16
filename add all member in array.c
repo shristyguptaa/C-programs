bc25c8bd47ccf3d3e3b404990a1c8a95da2042fc
Shristy Gupta <89759891+shristyguptaa@users.noreply.github.com>
2021-10-16 20:34:23 +0530

    Update add all member in array.c

--- add all member in array.c
+++ add all member in array.c
@@ -1,5 +1,7 @@
 //write a program to add all members in array and show the output
+
 #include<stdio.h>
+
 int main(){
 	char name[10]={'T','I','Y','A','&','D','I','V','Y','A'};
 	for (int i=0;i<10;i++){
