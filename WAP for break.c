41a0a124881e945fdde7b5f24d65590ae3ba42d6
Shristy Gupta <89759891+shristyguptaa@users.noreply.github.com>
2021-10-25 18:52:39 +0530

    Update WAP for break.c

--- WAP for break.c
+++ WAP for break.c
@@ -1,7 +1,7 @@
 //WAP for break 
 #include<stdio.h>
 int main(){
-	for (int i=1;i<=10;i++){
+	for (int i=1; i<=10; i++){
 		printf("%d",i);
 		if (i==3){
 			printf("\nyour number has 3:not allowed\n");
