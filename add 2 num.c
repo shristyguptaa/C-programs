a6fe5483551d4557668718895de6a2860d259221
Shristy Gupta <89759891+shristyguptaa@users.noreply.github.com>
2021-11-11 12:02:23 +0530

    Create add 2 num.c

---
+++ add 2 num.c
@@ -0,0 +1,12 @@
+//write a program to add 2 numbers
+#include<stdio.h>
+void add(){
+	int a = 3;
+	int b = 4;
+	int c = a + b;
+	printf("%d + %d = %d\n",a,b,c);
+}
+int main(){
+	add();
+	return 0;
+}
