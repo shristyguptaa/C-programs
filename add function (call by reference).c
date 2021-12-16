00c0553cc76b1a5cd824db367e07ab413f547280
Shristy Gupta <89759891+shristyguptaa@users.noreply.github.com>
2021-12-16 22:09:17 +0530

    Create add function (call by reference).c

---
+++ add function (call by reference).c
@@ -0,0 +1,10 @@
+//write a program to add function (call by reference)
+#include<stdio.h>
+void add (int a,int b){
+	int c = a + b;
+	printf("%d + %d = %d\n",a,b,c);
+}
+int main (){
+	add(5,2);
+	return 0;
+}
