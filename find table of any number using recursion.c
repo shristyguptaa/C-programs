afc0617cb1bdaa5b4ac0452bbc6ba3a9abd7997b
Shristy Gupta <89759891+shristyguptaa@users.noreply.github.com>
2022-01-04 20:14:38 +0530

    Create find table of any number using recursion.c

---
+++ find table of any number using recursion.c
@@ -0,0 +1,15 @@
+//WAP to find table of any number using recursion 
+#include<stdio.h>
+int i = 1;
+void table(int a){
+	if (i <= 10 ){
+		printf("%d x %d = %d\n",a,i,a*i);
+		i++;
+		table(a);
+	}
+}
+
+int main(){
+	table(2);
+	return 0;
+}
