2b0727347350d5c17ecccf64b2e56fe58bac1f41
Shristy Gupta <89759891+shristyguptaa@users.noreply.github.com>
2022-01-04 20:27:37 +0530

    Create use sting.h.c

---
+++ use sting.h.c
@@ -0,0 +1,12 @@
+//WAP to use sting.h
+#include<stdio.h>
+#include<string.h>
+int main(){
+	char n[200] = "Tiya & Divya";
+	printf("%s\n",strlwr(n));   //convert string to lower case
+	printf("%s\n",strupr(n));   //convert string to upper case
+	printf("%s\n",strrev(n));   //reverse the string 
+	printf("%d\n",strlen(n));   //count the length of string
+	
+	return 0;
+	}
