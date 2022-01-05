1d42494590c2091336b888bf71e52f200e7b3f4d
Shristy Gupta <89759891+shristyguptaa@users.noreply.github.com>
2022-01-05 21:49:04 +0530

    Create use math.h.c

---
+++ use math.h.c
@@ -0,0 +1,12 @@
+//WAP to use math.h
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+	int deg = 30;
+	float radian = deg * (3.14/180);
+	printf("%f",sin(radian));      //take input in radian
+	
+	return 0;
+	
+}
