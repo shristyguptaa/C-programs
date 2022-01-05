237f3f9318fe99f48d4e6954a8bde82514ecc43e
Shristy Gupta <89759891+shristyguptaa@users.noreply.github.com>
2022-01-05 21:54:35 +0530

    Create use math.h2.c

---
+++ use math.h2.c
@@ -0,0 +1,14 @@
+//WAP to use math.h
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+	printf("%f\n",exp(log(5)));
+	printf("%f\n",log(10));
+	printf("%f\n",exp(2));
+	printf("%f\n",sqrt(16));
+	printf("%f",pow(20,5));
+	
+	return 0;
+	
+}
