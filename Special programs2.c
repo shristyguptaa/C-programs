a7af848cc3b08b0f7c300b9d81919ac6ad08bdb6
Shristy Gupta <89759891+shristyguptaa@users.noreply.github.com>
2022-01-06 21:07:37 +0530

    Create Special programs2.c

---
+++ Special programs2.c
@@ -0,0 +1,11 @@
+//Special programs2
+#include<stdio.h>
+//while(1)
+
+int main(){
+	if (5,25,8,4,0,1)  //the last element gets check for if condition
+	printf("Hello");
+	
+	else 
+	printf("hi");
+}
